2cc827a77ad22a7c91a2d407cff060cf7e2edced
Revanth M. Bontha <69347893+RevanthMBontha@users.noreply.github.com>
2023-02-15 13:38:24 +0530

    Update Questions to fetch from an external API

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main>
       <div className="container">
-        <h3>FAQ about login</h3>
+        <h2>FAQ about login</h2>
         <Questions />
       </div>
     </main>
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
 import SingleQuestion from './Question';
 
-const Questions = () => {
-  const [questions, setQuestions] = useState(data);
+const url =
+  'https://api-for-basic-projects.netlify.app/accordion_project/accordion_data.json';
 
-  return (
-    <section className="info">
-      {questions.map((question) => {
-        return <SingleQuestion key={question.id} {...question} />;
-      })}
-    </section>
-  );
+const Questions = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [questions, setQuestions] = useState([]);
+
+  const fetchFAQ = async () => {
+    const response = await fetch(url);
+    const faqQuestions = await response.json();
+    setQuestions(faqQuestions);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFAQ();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="info">
+        <h3 style={{ textAlign: 'center' }}>FAQ elements are loading...</h3>
+      </section>
+    );
+  } else {
+    return (
+      <section className="info">
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />;
+        })}
+      </section>
+    );
+  }
 };
 
 export default Questions;
